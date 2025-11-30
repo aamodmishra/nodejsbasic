43de7b47ec2e3d41ca5fa8087f753a1fa1fce416
Your Name~ <aamodmishra111@gmail.com>
2025-11-30 13:22:36 +0545

    worked on functions

---
+++ function.js
@@ -0,0 +1,18 @@
+//hi
+function greet(name) {
+  console.log("Hello, " + name);
+}
+greet("aamod")
+
+//trying function expression 
+
+const sayGoodbye = function(name) {
+  console.log("Goodbye, " + name);
+};
+
+sayGoodbye("aamod")
+
+//arrow function 
+const multiply=(a,b)=>a*b;
+multiply(2,4);
+console.log(multiply(2,4));
