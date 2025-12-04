9e1b5211f32821cea1028308a63ae6ddb8c06829
Your Name~ <aamodmishra111@gmail.com>
2025-12-04 10:10:17 +0545

    callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,22 +1,15 @@
+function hello(callbacks){
+    console.log("this is hello function ")
+    callbacks();
+}
+function  hii(){
+    console.log ("this is hii");
 
-function processUserInput(name, callback) {
-    console.log(`Processing input for: ${name}`);
-    
-    // Simulate some work...
-    setTimeout(() => {
-        // Execute the callback after 2 seconds
-        callback(name); 
-    }, 2000);
-    
-    console.log("Input processing initiated. Waiting for callback...");
 }
 
-// The callback function
-function sayGoodbye(username) {
-    console.log(`\n--- Callback Executed ---`);
-    console.log(`Goodbye, ${username}! Your request is complete.`);
+function wait(){
+    console.log ("this is wait ");
+
 }
 
-// Pass 'sayGoodbye' function as a callback
-processUserInput("Alex", sayGoodbye); 
-
+hello(wait);
